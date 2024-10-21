cd934340081c5018f09261ad70c505472e22113e
kevin <kevinkoltraka1011@gmail.com>
2024-10-21 15:08:44 +0200

    sdf

--- LAB_SOM/Esercitazione 1 - Pthreads/problemi/p1.c
+++ LAB_SOM/Esercitazione 1 - Pthreads/problemi/p1.c
@@ -15,37 +15,78 @@ typedef struct{
 
 sondaggio s;
 
+void inizializzaSondaggio(sondaggio* s) {
+    for(int i=0; i<K; i++) {
+        snprintf(s->film[i], 40, "Film %d", i+1);
+        s->voti[i] = 0;
+    }
+
+    s->num_pareri   = 0;
+    pthread_mutex_init(&(s->mutex), NULL); 
+}
+
 void stampaSondaggio(sondaggio s) {
     for(int i=0; i<K; i++) {
-        printf("FILM: %s ; VOTO: %d\n", film[k], )
+        printf("FILM: %s;\t VOTO: %d;\n", s.film[i], s.voti[i]);
     }
+
+    printf("\n");
 }
 
-void *thread1_process (void * arg) {
-    int k=1;
-
-    pthread_mutex_lock(&s.mutex); 
+void* vota(void* arg) {
+    //accesso alla struttura dati condivisa
+    pthread_mutex_lock(&(s.mutex)); /* prologo */
     
+    printf("Spettatore numero %d sta votando\n", (int)arg);
     for(int i=0; i<K; i++) {
-        voti[i] += i
-
-        printf("accessi di T1: %d\n", accessi1);    
+        s.voti[i] += 1+rand()%10;
     }
+    
+    stampaSondaggio(s);
+    s.num_pareri++;
+    printf("\t\tnum pareri: %d\n", s.num_pareri);
 
-    pthread_mutex_unlock(&s.mutex); /*epilogo */
+    pthread_mutex_unlock(&(s.mutex)); /*epilogo */
 
-    pthread_exit (0);
+    pthread_exit(NULL);
 }
 
-main() { 
-    pthread_t th1, th2;
+int main() { 
+    pthread_t threads[N];
+    int rj;
+    srand(time(0)); 
+    
     /* INIZIALIZZA STRUTTURA DATI E SEMAFORO */
+    inizializzaSondaggio(&s);
+    stampaSondaggio(s);
 
-    if (pthread_create(&th1, NULL, thread1_process, NULL) < 0) {
-        fprintf (stderr, "create error for thread 1\n");
-        exit(1);
+    for(int i=0; i<N; i++) {
+        if (pthread_create(&threads[i], NULL, vota, (void *)i) < 0) {
+            fprintf (stderr, "create error for thread 1\n");
+            exit(1);
+        }
+    }
+    
+    for(int i=0; i<N; i++) {
+        rj = pthread_join(threads[i], NULL);
+
+        if (rj)
+            printf("ERRORE join thread %ld codice %d\n", threads[i], rj);
     }
 
-    pthread_join (th1, NULL);
-    pthread_join (th2, NULL);
+    //thread hanno terminato
+    int max = 0;
+    char* maxFilm;
+
+    for(int i=0; i<K; i++) {
+        int voto_medio = s.voti[i] / s.num_pareri;
+        printf("FILM: %s;\t VOTO MEDIO: %d;\n", s.film[i], voto_medio);
+
+        if( voto_medio > max ) {
+            max = voto_medio;
+            maxFilm = s.film[i];
+        }
+    }
+
+    printf("\nFilm vincitore: %s!\n", maxFilm);
 }
