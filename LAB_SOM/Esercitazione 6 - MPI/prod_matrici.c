b79151135b6058576a135ef855356f04ba842883
kevin <kevinkoltraka1011@gmail.com>
2024-12-15 12:49:28 +0100

    asd

--- LAB_SOM/Esercitazione 6 - MPI/prod_matrici.c
+++ LAB_SOM/Esercitazione 6 - MPI/prod_matrici.c
@@ -3,7 +3,7 @@
 #include <mpi.h>
 #include <time.h>
 
-#define DIM 3
+#define DIM 6
 
 int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
@@ -23,10 +23,13 @@ int main(int argc, char* argv[]) {
         MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
     }
     
+
+
+
     // la matrice B deve essere visibile a tutti
     int mat_B[DIM][DIM];
     // sottoporzioni delle matrici assegnate ai vari nodi
-    int  my_A[DIM*DIM], my_C[DIM*DIM];
+    int  my_A[DIM*(DIM/process_size)], my_C[DIM*(DIM/process_size)];
     
     if (my_rank == 0) {
         int mat_A[DIM][DIM];
@@ -39,58 +42,75 @@ int main(int argc, char* argv[]) {
                 mat_B[i][j] = rand()%10 + 1;
             }
         }
+
         printf("\nOperando A:\n");
         for(int i=0; i<DIM; i++) {
             printf("\t[ ");
+
             for(int j=0; j<DIM; j++) {
                 printf("%d\t", mat_A[i][j]);
             }
+
             printf(" ]\n");
         }
 
         printf("\nOperando B:\n");
         for(int i=0; i<DIM; i++) {
             printf("\t[ ");
+
             for(int j=0; j<DIM; j++) {
                 printf("%d\t", mat_B[i][j]);
             }
+
             printf(" ]\n");
         }
 
-        MPI_Scatter(mat_A, DIM*DIM/process_size, MPI_INT, &my_A, DIM*DIM/process_size, MPI_INT, 0, MPI_COMM_WORLD);   
+        // DIM/process_size = numero di righe assegnate ad ogni processo
+        // Ogni riga ha poi DIM elementi
+        MPI_Scatter(mat_A, DIM*(DIM/process_size), MPI_INT, &my_A, DIM*(DIM/process_size), MPI_INT, 0, MPI_COMM_WORLD); // mittente
     }
     else {
-        MPI_Scatter(NULL, DIM*DIM/process_size, MPI_INT, &my_A, DIM*DIM/process_size, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Scatter(NULL, DIM*(DIM/process_size), MPI_INT, &my_A, DIM*(DIM/process_size), MPI_INT, 0, MPI_COMM_WORLD);  // destinatari
     }
+
+    // printf("\tsono :%d my_rank; ho ricevuto la riga che inizia per %d\n", my_rank, my_A[0]);
+
     // tutti eseguono il brodcast della matrice operando B
     MPI_Bcast(mat_B, DIM*DIM, MPI_INT, 0, MPI_COMM_WORLD);
     
-    //TODO: qua gli indici sono scazzati, aggiusta
     // ogni nodo calcola la sua porzione
-    for(int i=0; i < DIM*DIM/process_size; i++) {
-        for(int j=0; j<DIM; j++) {
-            // l'indice di riga per la matrice B può eccedere la dimensione
-            // della riga singola e quindi devo aggiungere un modulo
-            my_C[i] += my_A[i] * mat_B[i%DIM][j];   
+    for(int i=0; i < DIM/process_size; i++) {       // questo scorre le righe della sotto-matrice my_A
+        for(int k=0; k < DIM; k++) {                // questo scorre tutte le colonne della matriche B
+            my_C[i*DIM + k] = 0; 
+
+            for(int j=0; j < DIM; j++) {            // questo scorre gli elementi all'interno di una riga/colonna
+                my_C[i*DIM + k] += my_A[i*DIM + j] * mat_B[j][k];   
+                // printf("\t\tsono: %d; sto calcolando A[%d][%d] * B[%d][%d] = %d * %d => C[%d][%d]\n", my_rank, i, j, j, k,  my_A[i*DIM + j], mat_B[j][k], i, k);
+            }
         }
     }
+
+    printf("\tsono :%d my_rank; ho calcolato delle righe che iniziano per %d\n", my_rank, my_C[0]);
         
     //collettore 
     if (my_rank==0) {   
         int mat_C[DIM][DIM];
-        MPI_Gather(&my_C, DIM*DIM/process_size, MPI_INT, mat_C, DIM*DIM/process_size, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Gather(&my_C, DIM*(DIM/process_size), MPI_INT, mat_C, DIM*(DIM/process_size), MPI_INT, 0, MPI_COMM_WORLD);  // ricevo
 
         printf("\nRisultato C=A*B:\n");
         for(int i=0; i<DIM; i++) {
             printf("\t[ ");
+
             for(int j=0; j<DIM; j++) {
                 printf("%d\t", mat_C[i][j]);
             }
+
             printf(" ]\n");
         }
     }
-    else
-        MPI_Gather(&my_C, DIM*DIM/process_size, MPI_INT, NULL, 0, MPI_INT, 0, MPI_COMM_WORLD);
+    else {
+        MPI_Gather(&my_C, DIM*(DIM/process_size), MPI_INT, NULL, 0, MPI_INT, 0, MPI_COMM_WORLD);    // invio
+    }
     
     MPI_Finalize();
     
--- comandi_cineca.md
+++ comandi_cineca.md
@@ -1,9 +1,18 @@
-1)Per creare il certificato necessario per l'accesso
+1) Per creare il certificato necessario per l'accesso
 
 	step ssh login 'kevin.koltraka@studio.unibo.it' --provisioner cineca-hpc
 
-2)Per effettuare effettivamente il login
+2) Per effettuare effettivamente il login
 
 	ssh kkoltrak@login.g100.cineca.it
+
+3) Per copiare un sorgente
+
+	scp ./prod_matrici.c kkoltrak@login.g100.cineca.it:.
+
+4) Per compilare con MPI
+
+	module load autoload intelmpi
+ 	mpiicc -std=c99 sommavet.c –o sommavet
 
 
