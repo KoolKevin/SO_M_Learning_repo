f3dc1b86dcad6f6e96f491f5459c07c4354261f8
kevin <kevinkoltraka1011@gmail.com>
2025-01-30 15:15:08 +0100

    asd

--- HPC/Approfondimento pseudo-convoluzione/prova_calcoli_per_scatterv.c
+++ HPC/Approfondimento pseudo-convoluzione/prova_calcoli_per_scatterv.c
@@ -2,13 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <math.h>
 
-#define DEBUG 1
+#include "omp.h"
+
+
+// #define DEBUG 1
 
 #define RESET   "\x1b[0m"    // Resetta i colori
 #define RED     "\x1b[31m"   // Rosso
 
-void stampa_matrice(const double* mat, int dim) {
+void stampa_matrice_evidenziata(const double* mat, int dim) {
     for(int i=0; i<dim; i++) {
         for(int j=0; j<dim; j++) {
             if (i%2 == 0 && j%2 == 0)
@@ -21,6 +25,77 @@ void stampa_matrice(const double* mat, int dim) {
     }
 
     printf("\n");
+}
+
+void stampa_matrice(const double* mat, int dim) {
+    for(int i=0; i<dim; i++) {
+        for(int j=0; j<dim; j++) {
+            printf("\t%0.2f", mat[i*dim + j]);
+        }     
+
+        printf("\n");   
+    }
+
+    printf("\n");
+}
+
+void check_matrici_uguali(const double* mat_a, const double* mat_b, int dim) {
+    double epsilon = 1E-8;
+
+    for(int i=0; i<dim; i++) {
+        for(int j=0; j<dim; j++) {
+            if( fabs(mat_a[i*dim + j] - mat_b[i*dim + j]) > epsilon) {
+                printf("le matrici differiscono al campo [%d, %d]\n", i, j);
+                printf("\t%f != %f\n", mat_a[i*dim + j], mat_b[i*dim + j]);
+                
+                return;
+            } 
+        }     
+    }
+
+    printf("le matrici sono uguali\n");
+}
+
+double calcola_media_intorno(const double* mat_input, int dim_input, int riga, int colonna) {
+    double somma = 0.0;
+
+    /*
+        considero l'intorno 3x3 dell'elemento corrente
+    */
+    for(int i=-1; i<=1; i++) {
+        for(int j=-1; j<=1; j++) {
+            int riga_intorno = riga + i;
+            int colonna_intorno = colonna + j;
+
+            // controllo se sto considerando l'intorno di un elemento su un bordo della matrice
+            if( (riga_intorno<0 || riga_intorno>=dim_input) || (colonna_intorno<0 || colonna_intorno>=dim_input) ) {
+                somma += 0.0;
+            }
+            else {
+                somma += mat_input[riga_intorno*dim_input + colonna_intorno];
+            }
+        }
+    }
+
+    return somma/9;
+}
+
+double pseudo_convoluzione(const double* mat_input, double* mat_output, int dim_input, int dim_output) {
+    double start = omp_get_wtime();
+
+    // scorro gli elementi PARI della matrice di input
+    // es: (0; 0), (0; 2), ..., (2; 0), (2; 2), ...
+    for(int i=0; i<dim_input; i+=2) {
+        for(int j=0; j<dim_input; j+=2) {
+            // calcolo la media dell'intorno del punto corrente
+            double media_intorno = calcola_media_intorno(mat_input, dim_input, i, j);
+            mat_output[(i/2)*dim_output + (j/2)] = media_intorno;
+        }     
+    }
+
+    double end = omp_get_wtime();
+
+    return end-start;
 }
 
 
@@ -74,8 +149,31 @@ int main(int argc, char** argv) {
 
     #ifdef DEBUG
     printf("--- MATRICE INPUT ---\n");
-    stampa_matrice(mat_input, dim_matrix);
+    stampa_matrice_evidenziata(mat_input, dim_matrix);
     #endif
+
+    double elapsed_sequential = pseudo_convoluzione(mat_input, mat_risultato, dim_matrix, dim_risultato);
+    #ifdef DEBUG
+    printf("--- MATRICE RISULTATO SEQUENZIALE ---\n");
+    stampa_matrice(mat_risultato, dim_risultato);
+    #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     // array contenente il numero di elementi da mandare ai vari processi 
     int* send_counts = malloc(num_proc * sizeof(int));
@@ -115,7 +213,31 @@ int main(int argc, char** argv) {
         printf("\tinput_offsets: %d\n", input_offsets[i]);
     }
 
+    double* mat_risultato_scatterv = malloc(size_risultato);
+    for (int i = 0; i < size_risultato/sizeof(double); i++) {
+        mat_risultato_scatterv[i] = 0.0;
+    }
+
+    for(int i=0; i<num_proc; i++) {
+        for(int j=0; j<send_counts[i]; j++) {
+            int riga = (j+input_offsets[i]) / dim_matrix;
+            int colonna = (j+input_offsets[i]) % dim_matrix;
+
+            if(riga%2 == 0 && colonna%2 == 0) {
+                double media_intorno = calcola_media_intorno(mat_input, dim_matrix, riga, colonna);
+                mat_risultato_scatterv[(riga/2)*dim_risultato + (colonna/2)] = media_intorno;
+            }
+        }     
+    }
+
+    #ifdef DEBUG
+    printf("--- MATRICE RISULTATO SCATTERV ---\n");
+    stampa_matrice(mat_risultato_scatterv, dim_risultato);
+    #endif 
+
+    check_matrici_uguali(mat_risultato, mat_risultato_scatterv, dim_risultato);
 
     free(mat_input);
     free(mat_risultato);
+    free(mat_risultato_scatterv);
 }
